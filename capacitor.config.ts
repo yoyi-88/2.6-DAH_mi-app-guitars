083aa7ad890400d26d4484bf23b7537fce484395
unknown <ygomben2510@g.educaand.es>
2025-10-03 09:52:05 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mi-app-guitar',
+  webDir: 'www'
+};
+
+export default config;
